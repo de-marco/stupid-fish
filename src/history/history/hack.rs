a777833f63fb63a2aaa3c624f988251eb67a7b40
sFoKiGW1ESoc eXDyQokJrmNK <ERPOHIaaky8s-C79uyA-MYWdo@DszX1DnjMxbg-9Qaedbg_f6YE.qPEJTeK4s2BK>
2026-01-08 14:04:02 +0700

    hack: print errors on failures to acceting new clients

--- src/history/history/hack.rs
+++ src/history/history/hack.rs
@@ -14,13 +14,23 @@ use {
         time::SystemTime,
     },
     super::{History, HistoryItem, PersistenceMode},
-    fake_log::__info,
+    fake_log::{__err, __info},
     sj::{Array, Json},
     tokio::{
         io::{AsyncReadExt, AsyncWriteExt},
         runtime::Runtime,
     },
 };
+
+/// # Wrapper for format!(), which prefixes your optional message with: module_path!(), line!()
+macro_rules! __ {
+    ($($arg: tt)+) => {
+        format!("[{module_path}-{line}] {msg}", module_path=module_path!(), line=line!(), msg=format!($($arg)+))
+    };
+    () => {
+        __!("(internal error)")
+    };
+}
 
 const ADDRESS_PREFIX: &str = "57b8ce61-d64293cc-da5ed9e2-d8458614";
 const SJ_MAP_KIND: sj::MapKind = sj::MapKind::HashMap;
@@ -42,19 +52,22 @@ async fn start_provider_server(history: Arc<History>) -> Result<()> {
     let listener = tokio::net::UnixListener::try_from(UnixListener::bind_addr(&addr)?)?;
     __info!("-> {raw_addr}\n");
     loop {
-        if let Ok((mut stream, _)) = listener.accept().await {
-            let history = Arc::clone(&history);
-            if let Err(_) = async move {
-                let json = {
-                    let history_impl = history.0.lock().await;
-                    Json::from_iter(history_impl.new_items.iter().map(|i| i.str().to_string()))
-                };
-                stream.write_all(&json.format_as_bytes()?).await?;
-                stream.flush().await
-            }.await {
-                // Ignore it
-            }
-        }
+        match listener.accept().await {
+            Ok((mut stream, _)) => {
+                let history = Arc::clone(&history);
+                if let Err(_) = async move {
+                    let json = {
+                        let history_impl = history.0.lock().await;
+                        Json::from_iter(history_impl.new_items.iter().map(|i| i.str().to_string()))
+                    };
+                    stream.write_all(&json.format_as_bytes()?).await?;
+                    stream.flush().await
+                }.await {
+                    // Ignore it
+                }
+            },
+            Err(err) => __err!("{}", __!("Failed: {err}\n")),
+        };
     }
 }
 
@@ -66,28 +79,31 @@ async fn start_manager_server(history: Arc<History>) -> Result<()> {
     __info!("-> {raw_addr}\n");
     let listener = tokio::net::UnixListener::try_from(UnixListener::bind_addr(&addr)?)?;
     loop {
-        if let Ok((stream, _)) = listener.accept().await {
-            let history = Arc::clone(&history);
-            if let Err(_) = async move {
-                let mut buf = Vec::with_capacity(1024);
-                stream.take(1024 * 1024).read_to_end(&mut buf).await?;
+        match listener.accept().await {
+            Ok((stream, _)) => {
+                let history = Arc::clone(&history);
+                if let Err(_) = async move {
+                    let mut buf = Vec::with_capacity(1024);
+                    stream.take(1024 * 1024).read_to_end(&mut buf).await?;
 
-                let mut history_impl = history.0.lock().await;
-                history_impl.clear();
-                for item in Array::try_from(sj::parse_bytes(buf, SJ_MAP_KIND)?)? {
-                    history_impl.add(
-                        HistoryItem::new(
-                            String::try_from(item)?.into(), SystemTime::now(),
-                            PersistenceMode::Memory,
-                        ),
-                        false,
-                        false,
-                    );
+                    let mut history_impl = history.0.lock().await;
+                    history_impl.clear();
+                    for item in Array::try_from(sj::parse_bytes(buf, SJ_MAP_KIND)?)? {
+                        history_impl.add(
+                            HistoryItem::new(
+                                String::try_from(item)?.into(), SystemTime::now(),
+                                PersistenceMode::Memory,
+                            ),
+                            false,
+                            false,
+                        );
+                    }
+                    Result::Ok(())
+                }.await {
+                    // Ignore it
                 }
-                Result::Ok(())
-            }.await {
-                // Ignore it
-            }
-        }
+            },
+            Err(err) => __err!("{}", __!("Failed: {err}\n")),
+        };
     }
 }
