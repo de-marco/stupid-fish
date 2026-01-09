0ca998f00b3964c219d2a9dacc6d3ac64a4a3000
0PiUsLs2FvjQ GDQ1NTHDw9gn <VI4XKANAjir6-8G_s-3ae2ZfH@zt2tiTdyZ_s5-5BfCRyHkvED7.fypq0MYbh-mh>
2026-01-09 16:44:25 +0700

    history::history::hack: moved code into new sub module `server`

--- src/history/history/hack.rs
+++ src/history/history/hack.rs
@@ -3,19 +3,11 @@ extern crate alloc;
 use {
     alloc::sync::Arc,
     std::{
-        io::{BufReader, BufWriter, Read, Result, Write},
-        os::{
-            linux::net::SocketAddrExt,
-            unix::net::{SocketAddr, UnixListener},
-        },
-        path::MAIN_SEPARATOR,
-        process,
+        io::Result,
         thread,
-        time::SystemTime,
     },
-    super::{History, HistoryItem, PersistenceMode},
-    fake_log::{__err, __info},
-    sj::{Array, Json},
+    super::History,
+    self::server::Server,
 };
 
 /// # Wrapper for format!(), which prefixes your optional message with: module_path!(), line!()
@@ -28,70 +20,16 @@ macro_rules! __ {
     };
 }
 
-const ADDRESS_PREFIX: &str = "57b8ce61-d64293cc-da5ed9e2-d8458614";
-const SJ_MAP_KIND: sj::MapKind = sj::MapKind::HashMap;
+mod server;
 
 pub (super) fn start_servers(history: Arc<History>) -> Result<()> {
     match Arc::clone(&history) {
-        history => thread::spawn(move || start_provider_server(history)),
+        history => thread::spawn(move || Server::Provider.start(history)),
     };
-    thread::spawn(move || start_manager_server(history));
+    match Arc::clone(&history) {
+        history => thread::spawn(move || Server::Manager.start(history)),
+    };
+    thread::spawn(move || Server::CdHistoryProvider.start(history));
 
     Ok(())
-}
-
-fn start_provider_server(history: Arc<History>) -> Result<()> {
-    let raw_addr = format!("{}{MAIN_SEPARATOR}{ADDRESS_PREFIX}{MAIN_SEPARATOR}provider", process::id());
-    let listener = UnixListener::bind_addr(&SocketAddr::from_abstract_name(&raw_addr)?)?;
-    __info!("-> {raw_addr}\n");
-    loop {
-        match listener.accept() {
-            Ok((stream, _)) => {
-                let history = Arc::clone(&history);
-                thread::spawn(move || {
-                    let json = {
-                        let history_impl = history.imp();
-                        Json::from_iter(history_impl.new_items.iter().map(|i| i.str().to_string()))
-                    };
-                    let mut stream = BufWriter::new(stream);
-                    stream.write_all(&json.format_as_bytes()?)?;
-                    stream.flush()
-                });
-            },
-            Err(err) => __err!("{}", __!("Failed: {err}\n")),
-        };
-    }
-}
-
-fn start_manager_server(history: Arc<History>) -> Result<()> {
-    let raw_addr = format!("{}{MAIN_SEPARATOR}{ADDRESS_PREFIX}{MAIN_SEPARATOR}manager", process::id());
-    __info!("-> {raw_addr}\n");
-    let listener = UnixListener::bind_addr(&SocketAddr::from_abstract_name(&raw_addr)?)?;
-    loop {
-        match listener.accept() {
-            Ok((stream, _)) => {
-                let history = Arc::clone(&history);
-                thread::spawn(move || {
-                    let stream = BufReader::new(stream);
-                    let mut buf = Vec::with_capacity(1024);
-                    stream.take(1024 * 1024).read_to_end(&mut buf)?;
-
-                    let mut history_impl = history.imp();
-                    history_impl.clear();
-                    for item in Array::try_from(sj::parse_bytes(buf, SJ_MAP_KIND)?)? {
-                        history_impl.add(
-                            HistoryItem::new(
-                                String::try_from(item)?.into(), SystemTime::now(),
-                                PersistenceMode::Memory,
-                            ),
-                            false,
-                            false,
-                        );
-                    }
-                    Result::Ok(())
-                });
-            },
-            Err(err) => __err!("{}", __!("Failed: {err}\n")),
-        };
-    }
 }
---
+++ src/history/history/hack/server.rs
@@ -0,0 +1,107 @@
+extern crate alloc;
+
+use {
+    alloc::sync::Arc,
+    std::{
+        io::{BufReader, BufWriter, Read, Result, Write},
+        os::{
+            linux::net::SocketAddrExt,
+            unix::net::{SocketAddr, UnixListener},
+        },
+        path::MAIN_SEPARATOR,
+        process,
+        thread,
+        time::SystemTime,
+    },
+    super::super::{History, HistoryItem, PersistenceMode},
+    fake_log::{__err, __info},
+    sj::{Array, Json},
+};
+
+const ADDRESS_PREFIX: &str = "57b8ce61-d64293cc-da5ed9e2-d8458614";
+const SJ_MAP_KIND: sj::MapKind = sj::MapKind::HashMap;
+
+#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq, Ord, PartialOrd)]
+pub (super) enum Server {
+    Provider,
+    Manager,
+    CdHistoryProvider,
+}
+
+impl Server {
+
+    pub const fn id(&self) -> &str {
+        match self {
+            Self::Provider => "provider",
+            Self::Manager => "manager",
+            Self::CdHistoryProvider => "cd-history-provider",
+        }
+    }
+
+    pub fn start(&self, history: Arc<History>) -> Result<()> {
+        let make_addr = || {
+            let raw_addr = format!("{process_id}{MAIN_SEPARATOR}{ADDRESS_PREFIX}{MAIN_SEPARATOR}{id}", process_id=process::id(), id=self.id());
+            let result = SocketAddr::from_abstract_name(&raw_addr)?;
+            __info!("-> {raw_addr}\n");
+            Result::Ok(result)
+        };
+        match self {
+            Self::Provider => start_provider_server(make_addr()?, history),
+            Self::Manager => start_manager_server(make_addr()?, history),
+            Self::CdHistoryProvider => todo!(),
+        }
+    }
+
+}
+
+fn start_provider_server(address: SocketAddr, history: Arc<History>) -> Result<()> {
+    let listener = UnixListener::bind_addr(&address)?;
+    loop {
+        match listener.accept() {
+            Ok((stream, _)) => {
+                let history = Arc::clone(&history);
+                thread::spawn(move || {
+                    let json = {
+                        let history_impl = history.imp();
+                        Json::from_iter(history_impl.new_items.iter().map(|i| i.str().to_string()))
+                    };
+                    let mut stream = BufWriter::new(stream);
+                    stream.write_all(&json.format_as_bytes()?)?;
+                    stream.flush()
+                });
+            },
+            Err(err) => __err!("{}", __!("Failed: {err}\n")),
+        };
+    }
+}
+
+fn start_manager_server(address: SocketAddr, history: Arc<History>) -> Result<()> {
+    let listener = UnixListener::bind_addr(&address)?;
+    loop {
+        match listener.accept() {
+            Ok((stream, _)) => {
+                let history = Arc::clone(&history);
+                thread::spawn(move || {
+                    let stream = BufReader::new(stream);
+                    let mut buf = Vec::with_capacity(1024);
+                    stream.take(1024 * 1024).read_to_end(&mut buf)?;
+
+                    let mut history_impl = history.imp();
+                    history_impl.clear();
+                    for item in Array::try_from(sj::parse_bytes(buf, SJ_MAP_KIND)?)? {
+                        history_impl.add(
+                            HistoryItem::new(
+                                String::try_from(item)?.into(), SystemTime::now(),
+                                PersistenceMode::Memory,
+                            ),
+                            false,
+                            false,
+                        );
+                    }
+                    Result::Ok(())
+                });
+            },
+            Err(err) => __err!("{}", __!("Failed: {err}\n")),
+        };
+    }
+}
