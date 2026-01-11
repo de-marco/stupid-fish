1c4ac22ab1a6e5c637d5f2245073acf19fa8f388
8DobF4Iukd9J Rjr3BVd1xmdn <N-SfXvulZ82b-D3TWvcOCQ-NY@3f-_gvl-CHLY--BJG6ufFGPpT.cvFHDnjLshLY>
2026-01-11 22:10:54 +0700

    Tests

--- src/builtins/cd/history/tests.rs
+++ src/builtins/cd/history/tests.rs
@@ -7,20 +7,27 @@ use {
 
 #[test]
 fn tests() -> Result<()> {
-    let mut hist = History::new(3);
+    const MAX_SIZE: usize = 3;
+
+    let mut hist = History::new(MAX_SIZE);
+
+    macro_rules! assert_max_size { () => {
+        assert_eq!(hist.by_time.len(), MAX_SIZE);
+        assert_eq!(hist.by_path.len(), MAX_SIZE);
+    }}
 
     hist.add("aaa");
     hist.add("bbb");
     hist.add("ccc");
-    assert_eq!(hist.len(), 3);
+    assert_max_size!();
 
     // Adding existing → updates time, doesn't grow size
     hist.add("bbb");
-    assert_eq!(hist.len(), 3);
+    assert_max_size!();
 
-    // Adding new → oldest ("a") should be removed
+    // Adding new → oldest ("aaa") should be removed
     hist.add("ddd");
-    assert_eq!(hist.len(), 3);
+    assert_max_size!();
 
     let recent: Vec<_> = hist.recents().map(|(_, p)| p).collect();
     assert_eq!(recent, vec!["ddd", "bbb", "ccc"]);
