0de30a159706f79a5d8cddc33ed96d08570699ef
DgMfxLJf4FjD nps4g7w6ACgU <rzHd07hokB3X-mdltVLoeIxdn@kHTK4mUCvF_k-ggr883VsLpg2.ASOCzyVj_kYf>
2026-01-13 14:03:50 +0700

    Optimized code

--- src/hack/proc_man/c_api.rs
+++ src/hack/proc_man/c_api.rs
@@ -1,9 +1,15 @@
+extern crate alloc;
+
 use {
-    core::ffi::{c_int, c_void},
+    core::{
+        ffi::{c_int, c_void},
+        time::Duration,
+    },
+    alloc::sync::Arc,
     std::{
-        os::unix::net::UnixDatagram,
         sync::OnceLock,
         thread,
+        time::Instant,
     },
     crate::hack::{MAP_KIND, Result},
     super::UNIX_DATAGRAM_BUF,
@@ -14,42 +20,59 @@ use {
     },
     libc::size_t,
     sj::Json,
+    tokio::{
+        net::UnixDatagram,
+        runtime::Runtime,
+    },
 };
 
 type Callback = unsafe extern "C" fn(*const u8, size_t, *const c_void);
 
-static THREAD: OnceLock<()> = OnceLock::new();
+static RUNTIME: OnceLock<Result<Arc<Runtime>>> = OnceLock::new();
 
 #[unsafe(no_mangle)]
 extern "C" fn stupid_fish_start_process_watcher(pid: u32, f: Callback, user_data: *const c_void) -> c_int {
-    let user_data = user_data as usize;
-    THREAD.get_or_init(move || drop(thread::spawn(move || {
-        if let Err(err) = (|| -> Result<()> {
-            let request = Nairud::from(Request::new(super::request::Request::WatchForProcesses, ())).encode_as_vec()?;
-
-            let stream = UnixDatagram::unbound()?;
-            stream.connect_addr(&super::super::make_socket_address_with(pid, super::UDS_STATUS_SERVER)?)?;
-            stream.send(&request)?;
-
-            loop {
-                let mut buf = [u8::MIN; UNIX_DATAGRAM_BUF];
-                let size = stream.recv(&mut buf)?;
-                let json = match Nairud::decode(&mut &buf[..size], MAP_KIND)? {
-                    Some(Nairud::Array(array)) => Json::from_iter([
-                        Json::from(u32::try_from(&array[usize::MIN])?),
-                        Json::from(array[1].as_str()?),
-                    ]),
-                    Some(Nairud::None) => sj::array(),
-                    other => return Err(err!("Unexpected data from server: {other:?}")),
-                };
-                let json = json.format_as_bytes()?;
-                unsafe {
-                    f(json.as_ptr(), json.len(), user_data as *const c_void);
-                }
-            }
-        })() {
-            __err!("{err}\n");
-        }
-    })));
-    0
+    thread::spawn(|| RUNTIME.get_or_init(|| Runtime::new().map(|r| Arc::new(r))));
+    let start_time = Instant::now();
+    while Instant::now().checked_duration_since(start_time).map(|d| d <= Duration::from_millis(100)).unwrap_or(false) {
+        match RUNTIME.get() {
+            None => thread::sleep(Duration::from_millis(10)),
+            Some(Err(err)) => {
+                __err!("{err}\n");
+                break;
+            },
+            Some(Ok(runtime)) => {
+                let user_data = user_data as usize;
+                runtime.spawn(async move {
+                    if let Result::<()>::Err(err) = async {
+                        let stream = std::os::unix::net::UnixDatagram::unbound()?;
+                        stream.connect_addr(&super::super::make_socket_address_with(pid, super::UDS_STATUS_SERVER)?)?;
+                        stream.set_nonblocking(true)?;
+                        let stream = UnixDatagram::try_from(stream)?;
+                        stream.send(&Nairud::from(Request::new(super::request::Request::WatchForProcesses, ())).encode_as_vec()?).await?;
+                        loop {
+                            let mut buf = [u8::MIN; UNIX_DATAGRAM_BUF];
+                            let size = stream.recv(&mut buf).await?;
+                            let json = match Nairud::decode(&mut &buf[..size], MAP_KIND)? {
+                                Some(Nairud::Array(array)) => Json::from_iter([
+                                    Json::from(u32::try_from(&array[usize::MIN])?),
+                                    Json::from(array[1].as_str()?),
+                                ]),
+                                Some(Nairud::None) => sj::array(),
+                                other => return Err(err!("Unexpected data from server: {other:?}")),
+                            };
+                            let json = json.format_as_bytes()?;
+                            unsafe {
+                                f(json.as_ptr(), json.len(), user_data as *const c_void);
+                            }
+                        }
+                    }.await {
+                        __err!("{err}\n");
+                    }
+                });
+                return 0;
+            },
+        };
+    }
+    -1
 }
