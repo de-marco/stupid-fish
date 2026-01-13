5db06a975d5a7f337b9de182561d736b6d96da7a
JV-U6hT6tdZu RpQnQ7ti5gqz <im_IbD3BB8gU-f7Y3Mgd2OjTi@p6PygzSAOclv-TC6mVkoJwppE.zBCfxGBVTFq0>
2026-01-13 08:05:51 +0700

    Optimized code

--- src/hack.rs
+++ src/hack.rs
@@ -58,9 +58,13 @@ pub fn bind<S>(id: S) -> Result<UnixListener> where S: AsRef<str> {
 }
 
 fn form_address<S>(id: S) -> String where S: AsRef<str> {
+    form_address_with(process::id(), id)
+}
+
+fn form_address_with<S>(process_id: u32, id: S) -> String where S: AsRef<str> {
     const ADDRESS_PREFIX: &str = "57b8ce61-d64293cc-da5ed9e2-d8458614";
 
-    let result = format!("{process_id}{MAIN_SEPARATOR}{ADDRESS_PREFIX}{MAIN_SEPARATOR}{id}", process_id=process::id(), id=id.as_ref());
+    let result = format!("{process_id}{MAIN_SEPARATOR}{ADDRESS_PREFIX}{MAIN_SEPARATOR}{id}", id=id.as_ref());
     __info!("-> {result}\n");
 
     result
